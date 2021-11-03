1c7a7fc7715a25e148f18e05acce5a70f5ea9b8c
Булава Кацярына <78858248+BulavaKatsyaryna@users.noreply.github.com>
2021-11-04 01:16:27 +0300

    Created a connection on the client side

--- Client/src/main/java/ClientLoader.java
+++ Client/src/main/java/ClientLoader.java
@@ -1,2 +1,26 @@
+import java.net.Socket;
+
 public class ClientLoader {
+
+    private static Socket socket;
+
+    public static void main(String[] args) {
+        connect();
+    }
+
+    private static void connect() {
+        try {
+            socket = new Socket("localhost", 8080);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void handle() {
+
+    }
+
+    private static void end() {
+
+    }
 }
